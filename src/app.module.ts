b0fe90a72abce039e764105f0dc2da7a6ef1c9cc
ryu820 <rhju820@naver.com>
2023-06-13 20:55:32 +0900

    [DAY2]service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MoviesController } from './movies/movies.controller';
+import { MoviesService } from './movies/movies.service';
 
 //데코레이터:클래스에 함수기능을 추가
 @Module({
   imports: [],
   controllers: [MoviesController],
-  providers: [],
+  providers: [MoviesService],
 })
 export class AppModule {}
--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -8,14 +8,18 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { Movie } from './entities/movie.entity';
 
 //()안에 있는 값이 url의 엔트리포인트를 컨트롤함
 //()안에 "movies"가 쓰여있으면 url은 /movies가 된다.
 @Controller()
 export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
+
   @Get()
-  getAll() {
-    return 'This is return all movies';
+  getAll(): Movie[] {
+    return this.moviesService.gerAll();
   }
 
   @Get('search')
@@ -24,18 +28,18 @@ export class MoviesController {
   }
 
   @Get(':id')
-  getOne(@Param('id') id: string) {
-    return `This will return One movie with the id : ${id}`;
+  getOne(@Param('id') id: string): Movie {
+    return this.moviesService.getOne(id);
   }
 
   @Post()
   create(@Body() movieData: string) {
-    return movieData;
+    return this.moviesService.create(movieData);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return `This will delete a movie with the id : ${id}`;
+    return this.moviesService.deleteOne(id);
   }
 
   @Patch(':id')
