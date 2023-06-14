5c7bab07dda3f18fc7133a9999698f8b1800b2a1
ryu820 <rhju820@naver.com>
2023-06-14 23:21:01 +0900

    [DAY3]모듈설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
-import { MoviesController } from './movies/movies.controller';
-import { MoviesService } from './movies/movies.service';
+// import { MoviesController } from './movies/movies.controller';
+// import { MoviesService } from './movies/movies.service';
+import { MoviesModule } from './movies/movies.module';
 
 //데코레이터:클래스에 함수기능을 추가
 @Module({
-  imports: [],
-  controllers: [MoviesController],
-  providers: [MoviesService],
+  imports: [MoviesModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
